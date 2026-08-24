c3a49dc96e396cae52e6fef859cbeada3801aa16
Eric Kowalewski <eric@thebeartraxs.com>
2026-08-24 13:32:12 -0400

    chore(release): v1.5.0 (bump)

--- soundwave.php
+++ soundwave.php
@@ -1,6 +1,6 @@
 <?php
 /*
- * Version: 1.4.42
+ * Version: 1.5.0
 Plugin Name: Soundwave
 Plugin URI: https://github.com/emkowale/soundwave
 Description: Pushes WooCommerce orders from affiliate/source sites to thebeartraxs.com (“hub”) via WooCommerce REST API.
@@ -12,7 +12,7 @@ License: GPL2
 */
 
 if ( ! defined('ABSPATH') ) exit;
-define('SOUNDWAVE_VERSION','1.4.42');
+define('SOUNDWAVE_VERSION','1.5.0');
 define('SOUNDWAVE_DIR', plugin_dir_path(__FILE__));
 define('SOUNDWAVE_URL', plugin_dir_url(__FILE__));
 
